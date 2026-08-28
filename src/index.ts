1d8b388f36caf5a7ed0497dd0fb692309d7ca3f7
zawzawl47qm-netizen <zawzawl47qm@gmail.com>
2026-08-28 22:41:36 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import path from "path"; 
 import express from "express";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const app = express();
